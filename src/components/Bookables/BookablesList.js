b963c5cb067c5897459b701c7f26021f2a12e01f
arief-github <ariefkurniawan0411@gmail.com>
2025-01-20 00:39:59 +0700

    show detail in checkbox

--- src/components/Bookables/BookablesList.js
+++ src/components/Bookables/BookablesList.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { bookables } from '../../static.json'
+import { bookables, sessions, days } from '../../static.json'
 import { FaArrowRight } from 'react-icons/fa'
 
 export default function BookablesList() {
@@ -8,6 +8,9 @@ export default function BookablesList() {
 
     const [bookableIndex, setBookableIndex] = useState(0)
     const nextBookable = () => setBookableIndex(i => (i + 1) % bookablesInGroup.length)
+
+    const bookable = bookablesInGroup[bookableIndex]
+    const [hasDetails, setHasDetails] = useState(false)
 
     const groups = [...new Set(bookables.map(b => b.group))]
 
@@ -28,10 +31,56 @@ export default function BookablesList() {
              </ul>
              <p>
                 <button className='btn' onClick={nextBookable} autoFocus>
-                    <FaArrowRight/>
                     <span>Next</span>
+                    <FaArrowRight/>
                 </button>
              </p>
+
+             {
+                bookable ? (
+                    <div className='bookable-details'>
+                        <div className='item'>
+                            <div className='item-header'>
+                                <h2>{bookable.title}</h2>
+                            </div>
+                            <span className='controls'>
+                                <label>
+                                    <input
+                                        type='checkbox'
+                                        checked={hasDetails}
+                                        onChange={() => setHasDetails(has => !has)}
+                                    />
+                                    Show Details
+                                </label>
+                            </span>
+                        </div>
+
+                        <p>{bookable.notes}</p>
+
+                        {
+                            hasDetails && (
+                                <div className='item-details'>
+                                    <h3>Availability</h3>
+                                    <div className='bookable-availability'>
+                                        <ul>
+                                            { bookable.days
+                                                .sort()
+                                                .map(d => <li key={d}> { days[d] } </li>)
+                                            }
+                                        </ul>
+                                        <ul>
+                                            {
+                                                bookable.sessions
+                                                    .map(s => <li key={s}>{sessions[s]}</li>)
+                                            }
+                                        </ul>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </div>
+                ) : null
+             }
         </>
     )
 }
